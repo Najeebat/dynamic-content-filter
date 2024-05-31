25034708e90bc793df254c16a60d8a4de26b72ba
Najeebat <isholanajeebatadedoyin@gmail.com>
2024-05-31 13:26:02 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function filterItems(category) {
+    const items = document.querySelectorAll('.item');
+
+    items.forEach(item => {
+        if (category === 'all' || item.classList.contains(category)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
